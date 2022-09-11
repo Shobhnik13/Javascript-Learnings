adeee0548f56a1eb03ba0b66548594b54f7c12b6
Shobhnik13 <shobhnikw@gmail.com>
2022-09-11 19:02:33 +0530

    commit

---
+++ arrays.js
@@ -0,0 +1,10 @@
+const marks=[1,2,3,4,5];
+const fruits=['abcd','xyz','efgh'];
+const mix=['hj',90,1,[3,5]]
+const arr=new Array(12,13,14,'klo');
+console.log(arr);
+console.log(fruits)
+console.log(mix)
+console.log(marks)
+console.log(arr.length);
+console.log(Array.isArray(arr));
---
+++ if else.js
@@ -0,0 +1,12 @@
+const age=17
+switch (age) {
+    case 18:
+     console.log('age is 18')   
+        break;
+case 20:
+    console.log('age is 20')
+    break;
+    default:
+        console.log('age is more than 20')
+        break;
+}
